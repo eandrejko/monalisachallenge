49541f7c05fd057e9afe669c1acfc3cbf8ee8549
Erik Andrejko <eandrejko@gmail.com>
2012-03-27 20:43:11 -0700

    added midje for tests

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.3.0-beta1"]
                            [noir-cljs "0.3.0"]]
+            :dev-dependencies [[midje "1.3.0"]]
             :main ^{:skip-aot true} monalisachallenge.server)
 
