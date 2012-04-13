6fa4eb47004f317d8ca10ece6fcf7f2a01c20f65
Erik Andrejko <andrejko@dory.local>
2012-04-12 20:46:21 -0700

    modified rendering to use anti-aliasing

--- src/monalisachallenge/rendering.clj
+++ src/monalisachallenge/rendering.clj
@@ -1,5 +1,5 @@
 (ns monalisachallenge.rendering
-  (:import [java.awt Graphics Color Polygon]
+  (:import [java.awt Graphics Color Polygon RenderingHints]
            [java.awt.image BufferedImage]
            [javax.imageio ImageIO]
            [java.io ByteArrayOutputStream ByteArrayInputStream]))
@@ -32,6 +32,9 @@
         [r g b]  (:background repr)
         img      (BufferedImage. width height BufferedImage/TYPE_INT_RGB)
         graphics (.createGraphics img)
+        graphics (doto graphics
+                   (.setRenderingHint RenderingHints/KEY_ANTIALIASING
+                                      RenderingHints/VALUE_ANTIALIAS_ON))
         graphics (doto graphics
                    (.setColor (Color. r g b))
                    (.fillRect 0 0 width height))]
